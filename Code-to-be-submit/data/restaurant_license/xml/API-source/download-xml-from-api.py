9007e7707910b51650ed2c5936a1f3b66a66dfd8
bkty1122 <kwoktszyi1122@gmail.com>
2024-04-05 23:57:24 +0800

    upload

---
+++ Code-to-be-submit/data/restaurant_license/xml/API-source/download-xml-from-api.py
@@ -0,0 +1,22 @@
+# download xml from API-source, if the last number 5 digit is 1 in the api url, it means the data is in the first day of the month
+import urllib.request
+import datetime
+import pandas as pd
+
+# default column name is 'api_url'
+api_source = pd.read_csv('api-source.csv', encoding='utf-8', names=['api_url'])
+
+# filter the api source which is in the first day of the month, by checking the last number 6 digit of the api url
+api_source = api_source[api_source['api_url'].str[-6] == '1']
+
+# export api source to csv file
+api_source.to_csv('api-source_1.csv', index=False, header=False)
+
+# download the xml file from api source, file name is the last 12 digit of the api url
+for index, row in api_source.iterrows():
+    url = row['api_url']
+    file_name = url[-12:]
+    urllib.request.urlretrieve(url, file_name + '.xml')
+    print(file_name + ' downloaded')
+    
+# python restaurant-info-hk\xml\download-xml-from-api.py
